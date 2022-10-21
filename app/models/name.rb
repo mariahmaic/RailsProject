254de40c949969c192ebe42b4099c3935c2f1447
Mariah <mariahmaic23@gmail.com>
2022-10-20 22:01:14 -0500

    Added name associations

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,6 +1,9 @@
 class Name < ApplicationRecord
   belongs_to :educator
-  has_many :educators
+  belongs_to :address
+  has_one :university
+  has_one :address
+  has_many :coursename
 
   def self.search_by(search_term)
     where("LOWER(name) LIKE :search_term", search_term: "%#{search_term.downcase}%")
