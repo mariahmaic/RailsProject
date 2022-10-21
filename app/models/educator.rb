036a63134d516975670afcb6fe8b69eee9444a18
Mariah <mariahmaic23@gmail.com>
2022-10-20 22:02:51 -0500

    Added educator associations

--- app/models/educator.rb
+++ app/models/educator.rb
@@ -1,4 +1,8 @@
 class Educator < ApplicationRecord
+belongs_to :name
+has_many :name
+has_many :city
+
   def self.search_by(search_term)
     where("LOWER(university) LIKE :search_word", search_word: "%#{search_word.downcase}%")
   end
